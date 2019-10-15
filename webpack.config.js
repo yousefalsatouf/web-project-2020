75e512009ef388ae4fb7a461e4b79efc8f2c9138
[Yousef Alsatouf] <yousef.alsatouf94@gmail.com>
2019-10-15 19:48:48 +0200

    getting started with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -23,17 +23,18 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/css/header.css')
-    //.addEntry('page1', './assets/js/page1.js')
-    //.addEntry('page2', './assets/js/page2.js')
+    .addStyleEntry('header', './assets/scss/header.scss')
+    .addStyleEntry('footer', './assets/scss/footer.scss')
+    //.addEntry('header', './assets/js/header.js')
+    //.addEntry('footer', './assets/js/footer.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
-    //.enableSingleRuntimeChunk()
-    .disableSingleRuntimeChunk()
+    .enableSingleRuntimeChunk()
+    //.disableSingleRuntimeChunk()
 
     /*
      * FEATURE CONFIG
@@ -55,7 +56,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
