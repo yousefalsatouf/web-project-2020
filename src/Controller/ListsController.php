398f608a084b261fec33d05277923b4528a37f8f
[Yousef Alsatouf] <yousef.alsatouf94@gmail.com>
2019-11-24 17:50:01 +0100

    working on the lists page

--- src/Controller/ListsController.php
+++ src/Controller/ListsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Prestataire;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,7 +13,10 @@ class ListsController extends AbstractController
      */
     public function index()
     {
+        $recipient = $this->getDoctrine()->getRepository(Prestataire::class);
+        $recipients = $recipient->findAll();
         return $this->render('lists/index.html.twig', [
+            'recipients' => $recipients,
             'lists_title' => 'Lists',
             'superlist' => 'Superlist',
             'links' => array('Home', 'Lists', 'Admin', 'Contact', 'Login', 'Register'),
