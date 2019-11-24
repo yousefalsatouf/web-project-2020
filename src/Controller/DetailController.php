d55b64d6f33e0f212870c966030f3e84189eb794
[Yousef Alsatouf] <yousef.alsatouf94@gmail.com>
2019-11-24 19:39:54 +0100

    detail page has been added

---
+++ src/Controller/DetailController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DetailController extends AbstractController
+{
+    /**
+     * @Route("/detail", name="detail")
+     */
+    public function index()
+    {
+        return $this->render('detail/index.html.twig', [
+            'controller_name' => 'Detail',
+
+        ]);
+    }
+}
